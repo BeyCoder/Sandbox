734bcea46be9e84c3c7f57c5b71b8f2c39929794
Beybut Abdulragimov <31037730+BeyCoder@users.noreply.github.com>
2022-04-23 22:25:42 +0400

    scrollspy effect

--- js/script.js
+++ js/script.js
@@ -37,6 +37,49 @@ document.addEventListener("DOMContentLoaded", function(){
             bsOffCanvas.hide();
         })
     });
+
+    var progressWrap = document.querySelector('.progress-wrap');
+    var progressPath = document.querySelector('.progress-wrap path');
+    var pathLength = progressPath.getTotalLength();
+    var offset = 50;
+    if (progressWrap != null) {
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+        window.addEventListener("scroll", function(event) {
+            var scroll = document.body.scrollTop || document.documentElement.scrollTop;
+            var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            var progress = pathLength - (scroll * pathLength / height);
+            progressPath.style.strokeDashoffset = progress;
+            var scrollElementPos = document.body.scrollTop || document.documentElement.scrollTop;
+            if (scrollElementPos >= offset) {
+                progressWrap.classList.add("active-progress")
+            } else {
+                progressWrap.classList.remove("active-progress")
+            }
+        });
+        progressWrap.addEventListener('click', function(e) {
+            e.preventDefault();
+            window.scroll({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            });
+        });
+    }
+
+    $(document).ready(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 110) {
+                $('.navbar-light').addClass("sticky-top slideOut navbar-stick");
+            } else {
+                $('.navbar-light').removeClass("sticky-top slideOut navbar-stick");
+            }
+        });
+    });
+
 // end if innerWidth
 });
 // DOMContentLoaded  end
